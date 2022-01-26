3c32263cef3d51826c79d46f1329ae93846f4553
Santiago Hermo <45404755+santiagohermo@users.noreply.github.com>
2022-01-26 13:50:57 -0500

    First version of run.py #1

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
 import os
 
+def main():
+
+    scripts = ['derived/code/make_images.py',
+               'derived/code/ocr.py',
+               'derived/code/clean.py',
+               'analysis/code/make_data.py',
+               'analysis/code/count.py']
+    
+    for script in scripts:
+        os.system(f'python {script}')
+
+
+if __name__ == '__main__':
+    main()
 
