4bba0575492edeef674b6609ca6c14f495e82f4a
Santiago Hermo <45404755+santiagohermo@users.noreply.github.com>
2022-01-27 14:47:31 -0500

    Adds logging to run.py #1 #2

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 def main():
 
@@ -8,8 +9,15 @@ def main():
                'analysis/code/make_data.py',
                'analysis/code/count.py']
     
+    log_file = open("run.log", 'w')
+    log_file.write(f"Compilation started at {time.strftime('%I:%M%p %Z on %b %d, %Y\n')}")
+        
     for script in scripts:
         os.system(f'python {script}')
+        log_file.write(f"{script} completed at {time.strftime('%I:%M%p %Z\n')}")
+    
+    log_file.write(f"Finished at {time.strftime('%I:%M%p %Z\n')}")
+    log_file.close()
 
 
 if __name__ == '__main__':
